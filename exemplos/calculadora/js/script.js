b7b11c994d0db7e6ec6d965abb8896f74fb59d77
Luiz Felipe <luizfelipe.freire@gmail.com>
2025-10-02 20:57:41 -0300

    aula 251002: Exemplo calculadora implementar a function: porcentagem

--- exemplos/calculadora/js/script.js
+++ exemplos/calculadora/js/script.js
@@ -24,3 +24,10 @@ function calcular() {
        visor.value = "Erro" 
     }
 }
+
+function porcentagem() {
+    calcular()
+    const visor = document.querySelector("#visor")
+    visor.value = Number(visor.value)/100
+}
+
