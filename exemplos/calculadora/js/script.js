43b72955a1eb944feb065a5e86e0126f0c726925
Luiz Felipe <luizfelipe.freire@gmail.com>
2025-10-02 20:24:51 -0300

    aula 251002: Exemplo calculadora implementar as functions: adicionar, limpar, apagar e calcular

---
+++ exemplos/calculadora/js/script.js
@@ -0,0 +1,26 @@
+// função para adicionar um valor ao visor
+function adicionar(valor){
+    document.querySelector("#visor").value += valor
+}
+
+// função para limpar o vistor
+function limpar(){
+    document.querySelector("#visor").value = ""
+}
+
+// função para apagar o último caractere
+function apagar(){
+    const visor = document.querySelector("#visor")
+    visor.value = visor.value.slice(0,-1)
+}
+
+// função para calcular o resultado
+function calcular() {
+    const visor = document.querySelector("#visor")
+    try {
+        // usar a function eval() para calcular o resultado
+        visor.value = eval(visor.value)
+    } catch (error) {
+       visor.value = "Erro" 
+    }
+}
