2062404ac63f0997fef0ed6b156a58e9b3e1ab96
Luiz Felipe <luizfelipe.freire@gmail.com>
2025-10-23 21:52:13 -0300

    aula 251023: Projeto ecommerce inclusão da funcionalidade dos produtos selecionados

--- exemplos/ecommerce/js/produtos.js
+++ exemplos/ecommerce/js/produtos.js
@@ -8,49 +8,49 @@ const lsProduto = [
     {
         nome: "Passeio na Floresta",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-02.png",
         valor: 39
     },
     {
         nome: "Trio Bom dia",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-03.png",
         valor: 39
     },
     {
         nome: "Meus Cristais",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-04.png",
         valor: 39
     },
     {
         nome: "Campo de Lavanda",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-05.png",
         valor: 39
     },
     {
         nome: "Minha Rotina",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-06.png",
         valor: 39
     },
     {
         nome: "Vida no Campo",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-07.png",
         valor: 39
     },
     {
         nome: "Meu Momento",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-08.png",
         valor: 39
     },
     {
         nome: "Flores Vivas",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-09.png",
         valor: 39
     }
 ]
