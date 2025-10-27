03c19088b833b3d76bc31583343792189acf56e7
Luiz Felipe <luizfelipe.freire@gmail.com>
2025-10-27 19:31:32 -0300

    aula 251027: Projeto ecommerce envio do pedido para o whatsapp

--- exemplos/ecommerce/js/script.js
+++ exemplos/ecommerce/js/script.js
@@ -92,5 +92,9 @@ frm.addEventListener("submit", (e) =>{
     let msg = "Desejo fazer o seguinte pedido!\n"
     msg += pedido
     msg += `Att: ${nome}`
-    alert(msg)
+    if(confirm("Deseja enviar essa mensagem?\n"+msg)){
+        msg = encodeURI(msg)
+        const link = `https://api.whatsapp.com/send?phone=5561985607460&text=${msg}`
+        window.open(link, '_blank')
+    }
 })
